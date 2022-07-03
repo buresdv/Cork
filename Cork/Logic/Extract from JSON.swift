0b383472285e6e588176fdb18e426650cda7565c
David Bureš <buresdv@gmail.com>
2022-07-03 19:26:13 +0200

    Added detail view

---
+++ Cork/Logic/Extract from JSON.swift
@@ -0,0 +1,21 @@
+//
+//  Extract from JSON.swift
+//  Cork
+//
+//  Created by David Bureš on 03.07.2022.
+//
+
+import Foundation
+
+enum WhatToExtract: String {
+    case description = "desc"
+    case homepage = "homepage"
+}
+
+func extractPackageInfo(rawJSON: String, whatToExtract: WhatToExtract) -> String {
+    let regex = "(?<=\(whatToExtract.rawValue)\": \").*?(?=\")"
+    guard let matchedRange = rawJSON.range(of: regex, options: .regularExpression) else { return "" }
+    let matchedString: String = String(rawJSON[matchedRange])
+    
+    return matchedString
+}
--- Cork/Views/Packages/Package Details.swift
+++ Cork/Views/Packages/Package Details.swift
@@ -18,13 +18,18 @@ struct PackageDetailView: View {
     
     @StateObject var packageInfo: SelectedPackageInfo
     
+    @State private var description: String = ""
+    @State private var homepage: String = ""
+    
     var body: some View {
-        VStack {
-            Text(package.name)
-                .font(.title)
-            Text(returnFormattedVersions(package.versions))
-                .font(.subheadline)
-                .foregroundColor(.gray)
+        VStack(alignment: .leading, spacing: 10) {
+            VStack(alignment: .leading) {
+                Text(package.name)
+                    .font(.title)
+                Text(returnFormattedVersions(package.versions))
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
             
             if packageInfo.contents == nil {
                 VStack {
@@ -32,12 +37,15 @@ struct PackageDetailView: View {
                     Text("Loading package info...")
                 }
             } else {
-                ScrollView {
-                    Text(packageInfo.contents!)
+                VStack(alignment: .leading) {
+                    Text(description)
+                    Text(.init(homepage))
                 }
             }
             
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+        .padding()
         .onAppear {
             Task {
                 if !isCask {
@@ -45,6 +53,9 @@ struct PackageDetailView: View {
                 } else {
                     packageInfo.contents = await shell("/opt/homebrew/bin/brew", ["info", "--json=v2", "--cask", package.name])
                 }
+                
+                description = extractPackageInfo(rawJSON: packageInfo.contents!, whatToExtract: .description)
+                homepage = extractPackageInfo(rawJSON: packageInfo.contents!, whatToExtract: .homepage)
                 
             }
         }
