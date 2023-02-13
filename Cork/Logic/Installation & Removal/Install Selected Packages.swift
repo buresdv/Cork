323fb6fdae1ecac5096daf884411233611b7edf7
buresdv <buresdv@gmail.com>
2023-02-13 01:04:10 +0100

    + Installing one package at a time works

--- Cork/Logic/Installation & Removal/Install Selected Packages.swift
+++ Cork/Logic/Installation & Removal/Install Selected Packages.swift
@@ -7,8 +7,53 @@
 
 import Foundation
 
+enum InstallationError: Error
+{
+    case outputHadErrors
+}
+
 @MainActor
-func installSelectedPackages(packageArray: [String], tracker: InstallationProgressTracker, brewData: BrewDataStorage) -> Void
+func installPackage(package: BrewPackage, installationProgressTracker: InstallationProgressTracker, brewData: BrewDataStorage) async throws -> TerminalOutput
+{
+    print("Installing package \(package.name)")
+
+    var installationResult: TerminalOutput
+
+    if !package.isCask
+    {
+        async let installationResultComplete: TerminalOutput = await shell("/opt/homebrew/bin/brew", ["install", package.name])
+        
+        print("Result for installing Formula \(package.name): \(await installationResultComplete)")
+
+        if await installationResultComplete.standardError.contains("error")
+        {
+            throw InstallationError.outputHadErrors
+        }
+        else
+        {
+            installationResult = await installationResultComplete
+        }
+    }
+    else
+    {
+        async let installationResultComplete: TerminalOutput = await shell("/opt/homebrew/bin/brew", ["install", "--cask", package.name])
+        
+        print("Result for installing Cask \(package.name): \(await installationResultComplete)")
+
+        if await installationResultComplete.standardError.contains("error")
+        {
+            throw InstallationError.outputHadErrors
+        }
+        else {
+            installationResult = await installationResultComplete
+        }
+    }
+
+    return installationResult
+}
+
+/*@MainActor
+func installSelectedPackages(packageArray: [String], tracker: InstallationProgressTracker, brewData _: BrewDataStorage)
 {
     let progressSteps = Float(1) / Float(packageArray.count)
     print(progressSteps)
@@ -33,7 +78,6 @@ func installSelectedPackages(packageArray: [String], tracker: InstallationProgre
             {
                 tracker.isShowingInstallationFailureAlert = true
             }
-            
         }
     }
-}
+}*/
--- Cork/Views/Installation/Install Package.swift
+++ Cork/Views/Installation/Install Package.swift
@@ -16,10 +16,9 @@ class SearchResultTracker: ObservableObject
 
 class InstallationProgressTracker: ObservableObject
 {
-    @Published var progressNumber: Float = 0
     @Published var packageBeingCurrentlyInstalled: String = ""
 
-    @Published var isShowingInstallationFailureAlert: Bool = false
+    @Published var packagesStillLeftToInstall: [String] = .init()
 }
 
 enum InstallationSteps
@@ -41,6 +40,8 @@ struct AddFormulaView: View
     @ObservedObject var installationProgressTracker = InstallationProgressTracker()
 
     @State var installationSteps: InstallationSteps = .ready
+    
+    @State var packageInstallTrackingNumber: Float = 0
 
     @FocusState var isSearchFieldFocused: Bool
 
@@ -161,15 +162,52 @@ struct AddFormulaView: View
                 }
 
             case .installing:
-                ProgressView(value: installationProgressTracker.progressNumber)
+                ProgressView(value: packageInstallTrackingNumber)
                 {
                     Text("Installing \(installationProgressTracker.packageBeingCurrentlyInstalled)")
                 }
                 .onAppear
                 {
                     for requestedPackage in foundPackageSelection {
-                        //print(getPackageNamesFromUUID(selectionBinding: requestedPackage, tracker: searchResultTracker))
-                        print(getPackageFromUUID(requestedPackageUUID: requestedPackage, tracker: searchResultTracker))
+                        // print(getPackageFromUUID(requestedPackageUUID: requestedPackage, tracker: searchResultTracker))
+                        
+                        let packageToInstall: BrewPackage = getPackageFromUUID(requestedPackageUUID: requestedPackage, tracker: searchResultTracker)
+                        
+                        installationProgressTracker.packagesStillLeftToInstall.append(packageToInstall.name)
+                        
+                        installationProgressTracker.packageBeingCurrentlyInstalled = packageToInstall.name
+                        
+                        Task(priority: .userInitiated) {
+                            do {
+                                // We have to do a little trolling to make the user feel like the program isn't frozen
+                                // After a random time up to 2s, move the progress line a little bit. I don't want them to think the program got stuck.
+                                // Slow-ass brew just doesn't install the packages fast enough
+                                DispatchQueue.main.asyncAfter(deadline: .now() + Double.random(in: 0...2)) {
+                                    packageInstallTrackingNumber = packageInstallTrackingNumber + Float.random(in: 0...0.1)
+                                }
+                                
+                                async let installationResult = try await installPackage(package: packageToInstall, installationProgressTracker: installationProgressTracker, brewData: brewData)
+                                
+                                print("Installation result: \(try await installationResult)")
+                                
+                                installationProgressTracker.packagesStillLeftToInstall.removeAll(where: { $0 == packageToInstall.name })
+                                
+                                if installationProgressTracker.packagesStillLeftToInstall.count == 0 {
+                                    packageInstallTrackingNumber = 1
+                                    
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 1)
+                                    {
+                                        installationSteps = .finished
+                                    }
+                                } else {
+                                    packageInstallTrackingNumber = Float(1 / installationProgressTracker.packagesStillLeftToInstall.count)
+                                }
+                            } catch let error as NSError {
+                                print("Error while installing package \(packageToInstall.name): \(error)")
+                            }
+                                        
+                        }
+                        
                     }
                 }
 
@@ -181,172 +219,5 @@ struct AddFormulaView: View
             }
         }
         .padding()
-
-        /* VStack(alignment: .leading)
-         {
-             Text("Install Packages")
-                 .font(.headline)
-                 .padding(.leading)
-             TextField("Search For Packages...", text: $packageRequested, onEditingChanged: { _ in
-                 foundPackageSelection = Set<UUID>() // Clear all selected items when the user looks for a different package
-             })
-             .padding(.horizontal)
-
-             if isShowingListLoader
-             {
-                 ProgressView()
-             }
-             else if installationProgressTracker.progressNumber != 0
-             {
-                 InstallProgressTrackerView(progress: $installationProgressTracker.progressNumber, currentlyInstallingPackage: $installationProgressTracker.packageBeingCurrentlyInstalled)
-             }
-             else if isShowingResultsList
-             {
-                 List(selection: $foundPackageSelection)
-                 {
-                     if !searchResultTracker.foundFormulae.isEmpty
-                     {
-                         Section("Found Formulae")
-                         {
-                             ForEach(searchResultTracker.foundFormulae)
-                             { formula in
-                                 HStack
-                                 {
-                                     Text(formula.packageName)
-                                     if brewData.installedFormulae.contains(where: { $0.name == formula.packageName })
-                                     {
-                                         PillText(text: "Already Installed")
-                                     }
-                                 }
-                             }
-                         }
-                     }
-
-                     if !searchResultTracker.foundCasks.isEmpty
-                     {
-                         Section("Found Casks")
-                         {
-                             ForEach(searchResultTracker.foundCasks)
-                             { cask in
-
-                                 HStack
-                                 {
-                                     Text(cask.packageName)
-                                     if brewData.installedCasks.contains(where: { $0.name == cask.packageName })
-                                     {
-                                         PillText(text: "Already Installed")
-                                     }
-                                 }
-
-                             }
-                         }
-                     }
-                 }
-                 .listStyle(.inset(alternatesRowBackgrounds: true))
-                 .frame(width: 300, height: 300)
-             }
-
-             HStack
-             {
-                 Button
-                 {
-                     isShowingSheet.toggle()
-                 } label: {
-                     Text("Cancel")
-                 }
-                 .keyboardShortcut(.cancelAction)
-
-                 Spacer()
-
-                 if foundPackageSelection.isEmpty
-                 {
-                     Button
-                     {
-                         isShowingResultsList = false
-                         searchResultTracker.foundFormulae = [SearchResult]()
-                         searchResultTracker.foundCasks = [SearchResult]()
-
-                         Task
-                         {
-                             isShowingListLoader = true
-                             print("Loader status: \(isShowingListLoader)")
-
-                             do
-                             {
-                                 let foundFormulae: [String] = try await searchForPackage(packageName: packageRequested, packageType: .formula)
-                                 let foundCasks: [String] = try await searchForPackage(packageName: packageRequested, packageType: .cask)
-
-                                 for formula in foundFormulae
-                                 {
-                                     searchResultTracker.foundFormulae.append(SearchResult(packageName: formula, isCask: false))
-                                 }
-
-                                 for cask in foundCasks
-                                 {
-                                     searchResultTracker.foundCasks.append(SearchResult(packageName: cask, isCask: true))
-                                 }
-                             }
-                             catch let packageRetrievalError as NSError
-                             {
-                                 print(packageRetrievalError)
-                             }
-
-                             isShowingListLoader = false
-                             isShowingResultsList = true
-                         }
-                     } label: {
-                         Text("Search")
-                     }
-                     .keyboardShortcut(.defaultAction)
-                 }
-                 else
-                 {
-                     HStack
-                     {
-                         Button
-                         {
-                             // TODO: Add logic that will show the user more information about the selected package
-
-                             let selectedPackages: [String] = getPackageNamesFromUUID(selectionBinding: foundPackageSelection, tracker: searchResultTracker)
-
-                             for selectedPackage in selectedPackages
-                             {
-                                 PackageDetailWindow(package: selectedPackage, tracker: searchResultTracker, brewData: brewData).openNewWindow(with: "Detail - \(selectedPackage)")
-                             }
-
-                         } label: {
-                             Text("More info")
-                         }
-                         .keyboardShortcut(.tab)
-
-                         Button
-                         {
-                             // TODO: Optimize this
-                             searchResultTracker.selectedPackagesForInstallation = [String]()
-
-                             installSelectedPackages(packageArray: getPackageNamesFromUUID(selectionBinding: foundPackageSelection, tracker: searchResultTracker), tracker: installationProgressTracker, brewData: brewData)
-
-                             print(searchResultTracker.selectedPackagesForInstallation)
-                         } label: {
-                             Text("Install")
-                         }
-                         .keyboardShortcut(.defaultAction)
-                     }
-                 }
-             }
-             .padding([.horizontal, .top])
-         }
-         .padding(.vertical)
-         .frame(width: 300)
-         .alert("Error installing package", isPresented: $installationProgressTracker.isShowingInstallationFailureAlert)
-         {
-             Button("Close", role: .cancel)
-             {
-                 installationProgressTracker.isShowingInstallationFailureAlert = false
-             }
-         } message: {
-             Text("An error occured while installing one of the selected packages.")
-             Text("Please try again in a few minutes")
-         } */
     }
 }
