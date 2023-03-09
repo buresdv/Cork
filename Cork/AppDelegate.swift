837817d3f05a01dde12d659c7a4a5152ce13f8a6
buresdv <buresdv@gmail.com>
2023-03-09 23:24:01 +0100

    ~ Cork terminates after last window closed

--- Cork/AppDelegate.swift
+++ Cork/AppDelegate.swift
@@ -11,6 +11,10 @@ import SwiftUI
 import DavidFoundation
 
 class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
     private var aboutWindowController: NSWindowController?
     
     func showAboutPanel()
