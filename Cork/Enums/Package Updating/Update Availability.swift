59748694dc3b7cf79b67e234ecf7dd5a4dab99df
buresdv <buresdv@gmail.com>
2023-03-12 13:08:43 +0100

    + Check for no updates available

---
+++ Cork/Enums/Package Updating/Update Availability.swift
@@ -0,0 +1,13 @@
+//
+//  Update Availability.swift
+//  Cork
+//
+//  Created by David Bureš on 12.03.2023.
+//
+
+import Foundation
+
+enum PackageUpdateAvailability
+{
+    case updatesAvailable, noUpdatesAvailable
+}
