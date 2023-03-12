b27407dc2033e0519bfc2345d52c4b3d73dfabf9
buresdv <buresdv@gmail.com>
2023-03-12 20:40:35 +0100

    + Custom list implementation + Searching in included formulae/casks

--- Cork/Views/Taps/Tap Details/Packages Included in Tap.swift
+++ Cork/Views/Taps/Tap Details/Packages Included in Tap.swift
@@ -11,13 +11,25 @@ struct PackagesIncludedInTapList: View {
     
     @State var packages: [String]
     
+    @State private var searchString: String = ""
+    
     var body: some View {
-        VStack
+        VStack(spacing: 0)
         {
-            List(packages, id: \.self)
-            { package in
-                Text(package)
+            TextField("Search...", text: $searchString)
+            ScrollView {
+                LazyVStack(spacing: 0) {
+                    ForEach(Array( searchString.isEmpty ? packages.enumerated() : packages.filter({ $0.contains(searchString) }).enumerated()), id: \.offset)
+                    { index, package in
+                        Text(package)
+                            .padding(6)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                            .background(index % 2 == 0 ? Color(nsColor: NSColor.alternatingContentBackgroundColors[0]) : Color(nsColor: NSColor.alternatingContentBackgroundColors[1]))
+                    }
+                }
             }
+            .frame(height: 150)
         }
+        .border(Color(nsColor: .lightGray))
     }
 }
