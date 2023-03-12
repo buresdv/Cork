8aa58c0a8e7a69ec15d6687d00a552be9a120921
buresdv <buresdv@gmail.com>
2023-03-12 12:48:41 +0100

    ~ Faster package updating

--- Cork/Logic/Installation & Removal/Install Selected Packages.swift
+++ Cork/Logic/Installation & Removal/Install Selected Packages.swift
@@ -24,9 +24,8 @@ func installPackage(installationProgressTracker: InstallationProgressTracker, br
 
     if !installationProgressTracker.packagesBeingInstalled[0].package.isCask
     {
-        
         print("Package is Formula")
-        
+
         for await output in shell(AppConstants.brewExecutablePath.absoluteString, ["install", installationProgressTracker.packagesBeingInstalled[0].package.name])
         {
             switch output
@@ -37,8 +36,8 @@ func installPackage(installationProgressTracker: InstallationProgressTracker, br
 
                 installationProgressTracker.packagesBeingInstalled[0].realTimeTerminalOutput?.append(outputLine)
 
-                    print("Does the line contain an element from the array? \(outputLine.containsElementFromArray(packageDependencies))")
-                    
+                print("Does the line contain an element from the array? \(outputLine.containsElementFromArray(packageDependencies))")
+
                 if outputLine.contains("Fetching dependencies")
                 {
                     // First, we have to get a list of all the dependencies
@@ -58,19 +57,19 @@ func installPackage(installationProgressTracker: InstallationProgressTracker, br
 
                     installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress = 1
                 }
-                    
-                    else if outputLine.contains("Already downloaded") || (outputLine.contains("Fetching") && outputLine.containsElementFromArray(packageDependencies))
+
+                else if outputLine.contains("Already downloaded") || (outputLine.contains("Fetching") && outputLine.containsElementFromArray(packageDependencies))
                 {
                     print("Will fetch dependencies!")
                     installationProgressTracker.packagesBeingInstalled[0].installationStage = .fetchingDependencies
-                    
+
                     installationProgressTracker.numberInLineOfPackageCurrentlyBeingFetched = installationProgressTracker.numberInLineOfPackageCurrentlyBeingFetched + 1
-                    
+
                     print("Fetching dependency \(installationProgressTracker.numberInLineOfPackageCurrentlyBeingFetched) of \(packageDependencies.count)")
-                    
+
                     installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress = installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress + Double(Double(10) / (Double(3) * (Double(installationProgressTracker.numberOfPackageDependencies) * Double(5))))
                 }
-                    
+
                 else if outputLine.contains("Installing dependencies") || outputLine.contains("Installing \(installationProgressTracker.packagesBeingInstalled[0].package.name) dependency")
                 {
                     print("Will install dependencies!")
@@ -83,14 +82,14 @@ func installPackage(installationProgressTracker: InstallationProgressTracker, br
                     // TODO: Add a math formula for advancing the stepper
                     installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress = installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress + Double(Double(10) / (Double(3) * Double(installationProgressTracker.numberOfPackageDependencies)))
                 }
-                    
+
                 else if outputLine.contains("Fetching \(installationProgressTracker.packagesBeingInstalled[0].package.name)") || outputLine.contains("Installing \(installationProgressTracker.packagesBeingInstalled[0].package.name)")
                 {
                     print("Will install the package itself!")
                     installationProgressTracker.packagesBeingInstalled[0].installationStage = .installingPackage
 
                     // TODO: Add a math formula for advancing the stepper
-                    installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress = Double(installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress) + Double( ( Double(10) - Double(installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress)) / Double(2))
+                    installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress = Double(installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress) + Double((Double(10) - Double(installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress)) / Double(2))
 
                     print("Stepper value: \(Double(Double(10) / (Double(3) * Double(installationProgressTracker.numberOfPackageDependencies))))")
                 }
@@ -105,72 +104,68 @@ func installPackage(installationProgressTracker: InstallationProgressTracker, br
         }
 
         installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress = 10
-        
+
         installationProgressTracker.packagesBeingInstalled[0].installationStage = .finished
-    
     }
     else
     {
         print("Package is Cask")
         print("Installing package \(installationProgressTracker.packagesBeingInstalled[0].package.name)")
-        
+
         for await output in shell(AppConstants.brewExecutablePath.absoluteString, ["install", "--no-quarantine", installationProgressTracker.packagesBeingInstalled[0].package.name])
         {
             switch output
             {
-                case let .standardOutput(outputLine):
-                    print("Output line: \(outputLine)")
-                    
-                    if outputLine.contains("Downloading")
-                    {
-                        print("Will download Cask")
-                        
-                        installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress = installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress + 2
-                        
-                        installationProgressTracker.packagesBeingInstalled[0].installationStage = .downloadingCask
-                        
-                        
-                    } else if outputLine.contains("Installing Cask")
-                    {
-                        print("Will install Cask")
-                        
-                        installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress = installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress + 2
-                        
-                        installationProgressTracker.packagesBeingInstalled[0].installationStage = .installingCask
-                        
-                        
-                    } else if outputLine.contains("Moving App")
-                    {
-                        print("Moving App")
-                        
-                        installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress = installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress + 2
-                        
-                        installationProgressTracker.packagesBeingInstalled[0].installationStage = .movingCask
-                    } else if outputLine.contains("Linking binary") {
-                        
-                        print("Linking Binary")
-                        
-                        installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress = installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress + 2
-                        
-                        installationProgressTracker.packagesBeingInstalled[0].installationStage = .linkingCaskBinary
-                        
-                    } else if outputLine.contains("was successfully installed")
-                    {
-                        print("Finished installing app")
-                        
-                        installationProgressTracker.packagesBeingInstalled[0].installationStage = .finished
-                        
-                        installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress = 10
-                        
-                    }
-                    
-                case let .standardError(errorLine):
-                    print("Line had error: \(errorLine)")
+            case let .standardOutput(outputLine):
+                print("Output line: \(outputLine)")
+
+                if outputLine.contains("Downloading")
+                {
+                    print("Will download Cask")
+
+                    installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress = installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress + 2
+
+                    installationProgressTracker.packagesBeingInstalled[0].installationStage = .downloadingCask
+                }
+                else if outputLine.contains("Installing Cask")
+                {
+                    print("Will install Cask")
+
+                    installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress = installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress + 2
+
+                    installationProgressTracker.packagesBeingInstalled[0].installationStage = .installingCask
+                }
+                else if outputLine.contains("Moving App")
+                {
+                    print("Moving App")
+
+                    installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress = installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress + 2
+
+                    installationProgressTracker.packagesBeingInstalled[0].installationStage = .movingCask
+                }
+                else if outputLine.contains("Linking binary")
+                {
+                    print("Linking Binary")
+
+                    installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress = installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress + 2
+
+                    installationProgressTracker.packagesBeingInstalled[0].installationStage = .linkingCaskBinary
+                }
+                else if outputLine.contains("was successfully installed")
+                {
+                    print("Finished installing app")
+
+                    installationProgressTracker.packagesBeingInstalled[0].installationStage = .finished
+
+                    installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress = 10
+                }
+
+            case let .standardError(errorLine):
+                print("Line had error: \(errorLine)")
             }
         }
-        
     }
-    
+
     await synchronizeInstalledPackages(brewData: brewData)
 
     return installationResult
