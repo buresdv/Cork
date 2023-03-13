b125cfc7ff314d21821adb7238256a3a337506cc
buresdv <buresdv@gmail.com>
2023-03-13 15:18:35 +0100

    ^ Maintenance padding

--- Cork/Views/Maintenance/Maintenance View.swift
+++ Cork/Views/Maintenance/Maintenance View.swift
@@ -251,12 +251,9 @@ struct MaintenanceView: View
                             .keyboardShortcut(.defaultAction)
                         }
                     }
-                    //.fixedSize(horizontal: false, vertical: true)
-                    .frame(width: 220, alignment: .leading)
-                    #warning("This is temporary, because it still cuts off some text. When I figure out a way to calculate the frame dynamically, change this")
+                    .fixedSize()
                 }
                 .padding()
-                //.frame(minWidth: 300, minHeight: 150)
                 .onAppear
                 {
                     Task
