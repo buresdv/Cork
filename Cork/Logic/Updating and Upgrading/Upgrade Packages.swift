4b6bbb1a1a19d4959cfa4a6d15a8150ed7c15cd9
buresdv <buresdv@gmail.com>
2023-03-16 15:27:48 +0100

    - Vestiges of old updating function

--- Cork/Logic/Updating and Upgrading/Upgrade Packages.swift
+++ Cork/Logic/Updating and Upgrading/Upgrade Packages.swift
@@ -45,31 +45,3 @@ func upgradePackages(_ updateProgressTracker: UpdateProgressTracker, appState _:
 
     updateProgressTracker.updateProgress = 10
 }
-
-/*
- @MainActor
- func updateBrewPackages(_ updateProgressTracker: UpdateProgressTracker, appState: AppState)
- {
-     Task
-     {
-         updateProgressTracker.updateProgress = 0
-
-         appState.isShowingUpdateSheet = true
-         updateProgressTracker.updateStage = .updating
-         updateProgressTracker.updateProgress += 0.2
-         let updateResult = await shell(AppConstants.brewExecutablePath.absoluteString, ["update"]).standardOutput
-         updateProgressTracker.updateProgress += 0.3
-
-         print("Update result: \(updateResult)")
-
-         updateProgressTracker.updateStage = .upgrading
-         updateProgressTracker.updateProgress += 0.2
-         let upgradeResult = await shell(AppConstants.brewExecutablePath.absoluteString, ["upgrade"]).standardOutput
-         updateProgressTracker.updateProgress += 0.3
-
-         print("Upgrade result: \(upgradeResult)")
-
-         appState.isShowingUpdateSheet = false
-     }
- }
- */
