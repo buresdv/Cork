2c259334c5c60cd9caec062bfd65ebaff25daa9a
David Bureš <buresdv@gmail.com>
2023-04-04 14:25:04 +0200

    ^ Formatting

--- Cork/Views/Installation/Install Package.swift
+++ Cork/Views/Installation/Install Package.swift
@@ -144,15 +144,14 @@ struct AddFormulaView: View
                                     let packageToInstall: BrewPackage = getPackageFromUUID(requestedPackageUUID: requestedPackage, tracker: searchResultTracker)
 
                                     installationProgressTracker.packagesBeingInstalled.append(PackageInProgressOfBeingInstalled(package: packageToInstall, installationStage: .ready, packageInstallationProgress: 0))
-                                    
+
                                     print("Packages to install: \(installationProgressTracker.packagesBeingInstalled)")
-                                    
+
                                     installationProgressTracker.packageBeingCurrentlyInstalled = packageToInstall.name
-                                    
                                 }
-                                
+
                                 print(installationProgressTracker.packagesBeingInstalled)
-                                
+
                                 packageInstallationProcessStep = .installing
                             } label: {
                                 Text("add-package.install.action")
@@ -166,47 +165,46 @@ struct AddFormulaView: View
             case .installing:
                 VStack(alignment: .leading)
                 {
-                    
                     ForEach(installationProgressTracker.packagesBeingInstalled)
                     { packageBeingInstalled in
-                        
+
                         if packageBeingInstalled.installationStage != .finished
                         {
                             ProgressView(value: installationProgressTracker.packagesBeingInstalled[0].packageInstallationProgress, total: 10)
                             {
                                 switch packageBeingInstalled.installationStage
                                 {
-                                    case .ready:
-                                        Text("add-package.install.ready")
-                                        
-                                    // FORMULAE
-                                    case .loadingDependencies:
-                                        Text("add-package.install.loading-dependencies")
-                                        
-                                    case .fetchingDependencies:
-                                        Text("add-package.install.fetching-dependencies")
-                                        
-                                    case .installingDependencies:
-                                        Text("add-package.install.installing-dependencies-\(installationProgressTracker.numberInLineOfPackageCurrentlyBeingInstalled)-of-\(installationProgressTracker.numberOfPackageDependencies)")
-                                        
-                                    case .installingPackage:
-                                        Text("add-package.install.installing-package")
-                                        
-                                    case .finished:
-                                        Text("add-package.install.finished")
-                                        
-                                    // CASKS
-                                    case .downloadingCask:
-                                        Text("add-package.install.downloading-cask-\(installationProgressTracker.packagesBeingInstalled[0].package.name)")
-                                        
-                                    case .installingCask:
-                                        Text("add-package.install.installing-cask-\(installationProgressTracker.packagesBeingInstalled[0].package.name)")
-                                        
-                                    case .linkingCaskBinary:
-                                        Text("add-package.install.linking-cask-binary")
-                                        
-                                    case .movingCask:
-                                        Text("add-package.install.moving-cask-\(installationProgressTracker.packagesBeingInstalled[0].package.name)")
+                                case .ready:
+                                    Text("add-package.install.ready")
+
+                                // FORMULAE
+                                case .loadingDependencies:
+                                    Text("add-package.install.loading-dependencies")
+
+                                case .fetchingDependencies:
+                                    Text("add-package.install.fetching-dependencies")
+
+                                case .installingDependencies:
+                                    Text("add-package.install.installing-dependencies-\(installationProgressTracker.numberInLineOfPackageCurrentlyBeingInstalled)-of-\(installationProgressTracker.numberOfPackageDependencies)")
+
+                                case .installingPackage:
+                                    Text("add-package.install.installing-package")
+
+                                case .finished:
+                                    Text("add-package.install.finished")
+
+                                // CASKS
+                                case .downloadingCask:
+                                    Text("add-package.install.downloading-cask-\(installationProgressTracker.packagesBeingInstalled[0].package.name)")
+
+                                case .installingCask:
+                                    Text("add-package.install.installing-cask-\(installationProgressTracker.packagesBeingInstalled[0].package.name)")
+
+                                case .linkingCaskBinary:
+                                    Text("add-package.install.linking-cask-binary")
+
+                                case .movingCask:
+                                    Text("add-package.install.moving-cask-\(installationProgressTracker.packagesBeingInstalled[0].package.name)")
                                 }
                             }
                         }
@@ -214,12 +212,11 @@ struct AddFormulaView: View
                         { // Show this when the installation is finished
                             Text("add-package.install.finished")
                                 .onAppear
-                            {
-                                packageInstallationProcessStep = .finished
-                            }
+                                {
+                                    packageInstallationProcessStep = .finished
+                                }
                         }
                     }
-                    
                 }
                 .onAppear
                 {
@@ -236,7 +233,8 @@ struct AddFormulaView: View
             case .finished:
                 DisappearableSheet(isShowingSheet: $isShowingSheet)
                 {
-                    ComplexWithIcon(systemName: "checkmark.seal") {
+                    ComplexWithIcon(systemName: "checkmark.seal")
+                    {
                         HeadlineWithSubheadline(
                             headline: "add-package.finished",
                             subheadline: "add-package.finished.description",
@@ -245,27 +243,29 @@ struct AddFormulaView: View
                     }
                 }
                 .onAppear
+                {
+                    appState.cachedDownloadsFolderSize = directorySize(url: AppConstants.brewCachedDownloadsPath)
+                }
+
+            default:
+                VStack(alignment: .leading)
+                {
+                    ComplexWithIcon(systemName: "wifi.exclamationmark")
                     {
-                        appState.cachedDownloadsFolderSize = directorySize(url: AppConstants.brewCachedDownloadsPath)
+                        HeadlineWithSubheadline(
+                            headline: "add-package.network-error",
+                            subheadline: "add-package.network-error.description",
+                            alignment: .leading
+                        )
                     }
-                    
-                default:
-                    VStack(alignment: .leading) {
-                        ComplexWithIcon(systemName: "wifi.exclamationmark") {
-                            HeadlineWithSubheadline(
-                                headline: "add-package.network-error",
-                                subheadline: "add-package.network-error.description",
-                                alignment: .leading
-                            )
-                        }
-                        
-                        HStack {
-                            Spacer()
-                            
-                            DismissSheetButton(isShowingSheet: $isShowingSheet)
-                        }
+
+                    HStack
+                    {
+                        Spacer()
+
+                        DismissSheetButton(isShowingSheet: $isShowingSheet)
                     }
-                    
+                }
             }
         }
         .padding()
