e70fd0260e73a2f3ddb50b28f85e29c25a0fe8d4
David Bureš <buresdv@gmail.com>
2023-04-29 20:04:42 +0200

    + Select / Deselect All button

--- Cork/Views/Start Page/Sub-Views/Outdated Package List Box.swift
+++ Cork/Views/Start Page/Sub-Views/Outdated Package List Box.swift
@@ -24,13 +24,15 @@ struct OutdatedPackageListBox: View
                     {
                         GroupBoxHeadlineGroupWithArbitraryContent(image: outdatedPackageTracker.outdatedPackages.count == 1 ? "square.and.arrow.down" : "square.and.arrow.down.on.square")
                         {
-                            VStack(alignment: .leading, spacing: 0) {
-                                HStack(alignment: .firstTextBaseline) {
+                            VStack(alignment: .leading, spacing: 0)
+                            {
+                                HStack(alignment: .firstTextBaseline)
+                                {
                                     Text(String.localizedPluralString("start-page.updates.count", outdatedPackageTracker.outdatedPackages.count))
                                         .font(.headline)
-                                    
+
                                     Spacer()
-                                    
+
                                     if outdatedPackageTracker.outdatedPackages.filter({ $0.isMarkedForUpdating }).count == outdatedPackageTracker.outdatedPackages.count
                                     {
                                         Button
@@ -39,38 +41,75 @@ struct OutdatedPackageListBox: View
                                         } label: {
                                             Text("start-page.updates.action")
                                         }
-                                    } else {
-                                        Button {
+                                    }
+                                    else
+                                    {
+                                        Button
+                                        {
                                             appState.isShowingIncrementalUpdateSheet = true
                                         } label: {
-                                            Text("Update \(outdatedPackageTracker.outdatedPackages.filter({ $0.isMarkedForUpdating }).count) Packages")
+                                            Text("Update \(outdatedPackageTracker.outdatedPackages.filter { $0.isMarkedForUpdating }.count) Packages")
                                         }
-                                        .disabled(outdatedPackageTracker.outdatedPackages.filter({ $0.isMarkedForUpdating }).count == 0)
-
+                                        .disabled(outdatedPackageTracker.outdatedPackages.filter { $0.isMarkedForUpdating }.count == 0)
                                     }
-                                    
                                 }
-                                
+
                                 DisclosureGroup
                                 {
                                     List
                                     {
-                                        ForEach(outdatedPackageTracker.outdatedPackages, id: \.self)
-                                        { outdatedPackage in
-                                            Toggle(outdatedPackage.package.name, isOn: Binding<Bool>(
-                                                get: {
-                                                    return outdatedPackage.isMarkedForUpdating
-                                                }, set: {
-                                                    if let index = outdatedPackageTracker.outdatedPackages.firstIndex(where: { $0.id == outdatedPackage.id })
-                                                    {
-                                                        outdatedPackageTracker.outdatedPackages[index].isMarkedForUpdating = $0
+                                        Section
+                                        {
+                                            ForEach(outdatedPackageTracker.outdatedPackages, id: \.self)
+                                            { outdatedPackage in
+                                                Toggle(outdatedPackage.package.name, isOn: Binding<Bool>(
+                                                    get: {
+                                                        outdatedPackage.isMarkedForUpdating
+                                                    }, set: {
+                                                        if let index = outdatedPackageTracker.outdatedPackages.firstIndex(where: { $0.id == outdatedPackage.id })
+                                                        {
+                                                            outdatedPackageTracker.outdatedPackages[index].isMarkedForUpdating = $0
+                                                        }
                                                     }
+                                                ))
+                                            }
+                                        } header: {
+                                            HStack(alignment: .center, spacing: 10)
+                                            {
+                                                Button
+                                                {
+                                                    for outdatedPackage in outdatedPackageTracker.outdatedPackages
+                                                    {
+                                                        if let index = outdatedPackageTracker.outdatedPackages.firstIndex(where: { $0.id == outdatedPackage.id })
+                                                        {
+                                                            outdatedPackageTracker.outdatedPackages[index].isMarkedForUpdating = false
+                                                        }
+                                                    }
+                                                } label: {
+                                                    Text("start-page.updated.action.deselect-all")
                                                 }
-                                            ))
+                                                .buttonStyle(.plain)
+                                                .disabled(outdatedPackageTracker.outdatedPackages.filter({ $0.isMarkedForUpdating }).count == 0)
+
+                                                Button
+                                                {
+                                                    for outdatedPackage in outdatedPackageTracker.outdatedPackages
+                                                    {
+                                                        if let index = outdatedPackageTracker.outdatedPackages.firstIndex(where: { $0.id == outdatedPackage.id })
+                                                        {
+                                                            outdatedPackageTracker.outdatedPackages[index].isMarkedForUpdating = true
+                                                        }
+                                                    }
+                                                } label: {
+                                                    Text("start-page.updated.action.select-all")
+                                                }
+                                                .buttonStyle(.plain)
+                                                .disabled(outdatedPackageTracker.outdatedPackages.filter { $0.isMarkedForUpdating }.count == outdatedPackageTracker.outdatedPackages.count)
+                                            }
                                         }
                                     }
                                     .listStyle(.bordered(alternatesRowBackgrounds: true))
-                                    .frame(height: 100)
+                                    .frame(height: 120)
                                 } label: {
                                     Text("start-page.updates.list")
                                         .font(.subheadline)
