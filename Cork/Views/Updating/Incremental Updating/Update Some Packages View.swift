40b44254fc07a44b39a2d4fc83b10952afaf2664
David Bureš <buresdv@gmail.com>
2023-04-29 22:11:56 +0200

    + Some views for incremental updates

--- Cork/Views/Updating/Incremental Updating/Update Some Packages View.swift
+++ Cork/Views/Updating/Incremental Updating/Update Some Packages View.swift
@@ -7,24 +7,47 @@
 
 import SwiftUI
 
-struct UpdateSomePackagesView: View {
-    
+struct UpdateSomePackagesView: View
+{
     @EnvironmentObject var outdatedPackageTracker: OutdatedPackageTracker
-    
+
     @Binding var isShowingSheet: Bool
-    
+
     @State private var packageUpdatingStage: PackageUpdatingStage = .updating
-    
-    var selectedPackages: [OutdatedPackage] {
-        return outdatedPackageTracker.outdatedPackages.filter({ $0.isMarkedForUpdating })
+    @State private var packageBeingCurrentlyUpdated: BrewPackage?
+
+    var selectedPackages: [OutdatedPackage]
+    {
+        return outdatedPackageTracker.outdatedPackages.filter { $0.isMarkedForUpdating }
     }
-    
-    var body: some View {
+
+    var body: some View
+    {
         VStack(alignment: .leading, spacing: 10)
         {
-            Text("Would update:")
-            ForEach(selectedPackages) { outdatedPackage in
-                Text("\(outdatedPackage.package.name) - \(outdatedPackage.package.isCask ? "Cask" : "Formula")")
+            switch packageUpdatingStage
+            {
+            case .updating:
+                ProgressView(value: 1, total: Double(outdatedPackageTracker.outdatedPackages.count))
+                {
+                    Text("Ahoj")
+                }
+            case .finished:
+                DisappearableSheet(isShowingSheet: $isShowingSheet)
+                {
+                    ComplexWithIcon(systemName: "checkmark.seal")
+                    {
+                        SheetWithTitle(title: "update-packages.incremental.finished")
+                        {
+                            Text("update-packages.finished.description")
+                        }
+                    }
+                }
+
+            case .erroredOut:
+                Text("Errors")
+            case .noUpdatesAvailable:
+                Text("update-packages.incremental.impossible-case")
             }
         }
         .padding()
