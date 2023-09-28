915c5696098dbe9a51048a0b508c0fd7bb0e3bd3
David Bureš <buresdv@gmail.com>
2023-09-28 15:52:06 +0200

    + First iteration of new details page

---
+++ Cork/Views/Packages/Package Details/Sub-Views/Package Dependencies.swift
@@ -0,0 +1,18 @@
+//
+//  Package Dependencies.swift
+//  Cork
+//
+//  Created by David Bureš on 28.09.2023.
+//
+
+import SwiftUI
+
+struct Package_Dependencies: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Package_Dependencies()
+}
