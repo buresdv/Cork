1adb0178efea0823c64b4bb51d5b22ebde97622b
David Bureš <buresdv@gmail.com>
2023-10-03 23:14:54 +0200

    + Func for loading local images from URL

--- Cork/Views/Reusables/Complex with Icon.swift
+++ Cork/Views/Reusables/Complex with Icon.swift
@@ -26,3 +26,23 @@ struct ComplexWithIcon<Content: View>: View
         }
     }
 }
+
+struct ComplexWithIconWithSystemImage<Content: View>: View
+{
+    @State var imageName: NSImage.Name
+
+    @ViewBuilder var content: Content
+
+    var body: some View
+    {
+        HStack(alignment: .top, spacing: 10)
+        {
+            Image(nsImage: NSImage(named: imageName)!)
+                .resizable()
+                .frame(width: 50, height: 50)
+                .foregroundColor(.secondary)
+
+            content
+        }
+    }
+}
