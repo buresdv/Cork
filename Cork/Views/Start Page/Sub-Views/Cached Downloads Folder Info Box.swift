982a576130fe7835bc5dac4dd373459238cea818
David Bureš <buresdv@gmail.com>
2023-11-04 15:36:20 +0100

    + First Chart implementation

--- Cork/Views/Start Page/Sub-Views/Cached Downloads Folder Info Box.swift
+++ Cork/Views/Start Page/Sub-Views/Cached Downloads Folder Info Box.swift
@@ -6,10 +6,21 @@
 //
 
 import SwiftUI
+import Charts
 
 struct CachedDownloadsFolderInfoBox: View
 {
+    private struct CachedDownload: Identifiable, Hashable {
+        
+        var id: String { packageName }
+        
+        let packageName: String
+        let sizeInBytes: Int
+    }
+    
     @EnvironmentObject var appState: AppState
+    
+    @State private var cachedDownloads: Set<CachedDownload> = .init()
 
     var body: some View
     {
@@ -17,11 +28,27 @@ struct CachedDownloadsFolderInfoBox: View
         {
             HStack
             {
-                GroupBoxHeadlineGroup(
-                    image: "archivebox",
-                    title: "start-page.cached-downloads-\(appState.cachedDownloadsFolderSize.formatted(.byteCount(style: .file)))",
-                    mainText: "start-page.cached-downloads.description"
-                )
+                VStack(alignment: .leading)
+                {
+                    GroupBoxHeadlineGroup(
+                        image: "archivebox",
+                        title: "start-page.cached-downloads-\(appState.cachedDownloadsFolderSize.formatted(.byteCount(style: .file)))",
+                        mainText: "start-page.cached-downloads.description"
+                    )
+                    
+                    if !cachedDownloads.isEmpty
+                    {
+                        Chart(cachedDownloads.sorted(by: { $0.sizeInBytes < $1.sizeInBytes }))
+                        {
+                            BarMark(
+                                x: .value("start-page.cached-downloads.graph.size", $0.sizeInBytes)
+                            )
+                            .foregroundStyle(by: .value("start-page.cached-downloads.graph.package-name", $0.packageName))
+                        }
+                        .chartXAxis(.hidden)
+                        .chartYScale(type: .log)
+                    }
+                }
 
                 Spacer()
 
@@ -33,5 +60,43 @@ struct CachedDownloadsFolderInfoBox: View
                 }
             }
         }
+        .task(priority: .background) 
+        {
+            await loadCachedDownloads()
+        }
+    }
+    
+    private func loadCachedDownloads() async
+    {
+        guard let cachedDownloadsFolderContents: [URL] = try? FileManager.default.contentsOfDirectory(at: AppConstants.brewCachedDownloadsPath, includingPropertiesForKeys: [.isRegularFileKey], options: [.skipsHiddenFiles]) else
+        {
+            return
+        }
+        
+        let usableCachedDownloads: [URL] = cachedDownloadsFolderContents.filter({ $0.pathExtension != "json" })
+        
+        for usableCachedDownload in usableCachedDownloads 
+        {
+            guard let itemName: String = try? regexMatch(from: usableCachedDownload.lastPathComponent, regex: "(?<=--)(.*?)(?=\\.)") else
+            {
+                return
+            }
+            
+            print("Temp item name: \(itemName)")
+            
+            guard let itemAttributes = try? FileManager.default.attributesOfItem(atPath: usableCachedDownload.path) else
+            {
+                return
+            }
+            
+            guard let itemSize = itemAttributes[.size] as? Int else
+            {
+                return
+            }
+            
+            cachedDownloads.insert(.init(packageName: itemName, sizeInBytes: itemSize))
+        }
+        
+        print(print("Cached downloads contents: \(cachedDownloads)"))
     }
 }
