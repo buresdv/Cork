82bd955ffcb23d778eb54b0730bfc0e0bc4a6321
David Bureš <buresdv@gmail.com>
2023-12-07 11:11:00 +0100

    + Tap name no longer in the name of some packages

--- Cork/Views/Shared/Sanitized Package Name.swift
+++ Cork/Views/Shared/Sanitized Package Name.swift
@@ -12,12 +12,31 @@ struct SanitizedPackageName: View
 {
     let packageName: String
     @State var shouldShowVersion: Bool
+    
+    var packageNameWithoutTapName: String
+    {
+        if packageName.contains("/")
+        { /// Check if the package name contains slashes (this would mean it includes the tap name)
+            if let sanitizedName = try? regexMatch(from: packageName, regex: "[^\\/]*$")
+            {
+                return sanitizedName
+            }
+            else
+            {
+                return packageName
+            }
+        }
+        else
+        {
+            return packageName
+        }
+    }
 
     var body: some View
     {
-        if packageName.contains("@")
+        if packageNameWithoutTapName.contains("@")
         { /// Only do the matching if the name contains @
-            if let sanitizedName = try? regexMatch(from: packageName, regex: ".+?(?=@)")
+            if let sanitizedName = try? regexMatch(from: packageNameWithoutTapName, regex: ".+?(?=@)")
             { /// Try to REGEX-match the name out of the raw name
                 HStack(alignment: .firstTextBaseline, spacing: 5) {
                     Text(sanitizedName)
@@ -25,7 +44,7 @@ struct SanitizedPackageName: View
                     if shouldShowVersion
                     {
                         /// The version is the lenght of the package name, + 1 due to the @ character
-                        Text("v. \(String(packageName.dropFirst(sanitizedName.count + 1)))")
+                        Text("v. \(String(packageNameWithoutTapName.dropFirst(sanitizedName.count + 1)))")
                             .foregroundColor(.gray)
                             .font(.subheadline)
                     }
@@ -33,12 +52,12 @@ struct SanitizedPackageName: View
             }
             else
             { /// If the REGEX matching fails, just show the entire name
-                Text(packageName)
+                Text(packageNameWithoutTapName)
             }
         }
         else
         { /// If the name doesn't contain the @, don't do anything
-            Text(packageName)
+            Text(packageNameWithoutTapName)
         }
     }
 }
