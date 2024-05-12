ce5db9cf0537c43e300e1ec4d8367185f540b316
David Bureš <buresdv@gmail.com>
2024-05-12 20:48:03 +0200

    + More portable service replacement

--- Cork/Models/Services/Services Tracker.swift
+++ Cork/Models/Services/Services Tracker.swift
@@ -12,17 +12,40 @@ class ServicesTracker: ObservableObject
 {
     @Published var services: Set<HomebrewService> = .init()
 
-    func changeServiceStatus(_ serviceToChange: HomebrewService, newStatus: ServiceStatus)
-    {       
-        self.services = Set(self.services.map({ service in
+    /// Replace a service in the tracker.
+    /// Use `performInPlaceReplacement` to preserve the UUID of the original service so the UI doesn't completely reset
+    func replaceServiceInTracker(_ serviceToChange: HomebrewService, with newService: HomebrewService, performInPlaceReplacement: Bool)
+    {
+        services = Set(services.map
+        { service in
             var copyService = service
-            
+
             if copyService.name == serviceToChange.name
             {
-                copyService.status = newStatus
+                copyService = .init(
+                    id: performInPlaceReplacement ? copyService.id : newService.id,
+                    name: newService.name,
+                    status: newService.status,
+                    user: newService.user,
+                    location: newService.location,
+                    exitCode: newService.exitCode
+                )
             }
-            
+
             return copyService
-        }))
+        })
+    }
+
+    func changeServiceStatus(_ serviceToChange: HomebrewService, newStatus: ServiceStatus)
+    {
+        self.replaceServiceInTracker(
+            serviceToChange,
+            with: .init(
+                name: serviceToChange.name,
+                status: newStatus,
+                user: serviceToChange.user,
+                location: serviceToChange.location,
+                exitCode: serviceToChange.exitCode),
+            performInPlaceReplacement: true)
     }
 }
