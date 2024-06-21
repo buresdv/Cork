a2c5267592665c48e4de13eeb50cfd3d73486745
David Bureš <buresdv@gmail.com>
2024-06-21 18:17:31 +0200

    ~ File location

--- Cork/Logic/Package Loading/Get Outdated Packages.swift
+++ Cork/Logic/Package Loading/Get Outdated Packages.swift
@@ -87,7 +87,7 @@ extension OutdatedPackageTracker
             self.outdatedPackages = await finalOutdatedFormulae.union(finalOutdatedCasks)
             
         } catch let decodingError {
-            AppConstants.logger.error("There was an error decoding the outdated package retrieval output: \(decodingError.localizedDescription)")
+            AppConstants.logger.error("There was an error decoding the outdated package retrieval output: \(decodingError.localizedDescription)\n\(decodingError)")
             throw OutdatedPackageRetrievalError.couldNotDecodeCommandOutput(decodingError.localizedDescription)
         }
     }
