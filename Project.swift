46c9c52d46769a8d2cc297efae0e3b699d5b21dc
David Bureš <buresdv@gmail.com>
2024-07-25 09:22:06 +0200

    ~ Tuist configuration indentation

--- Project.swift
+++ Project.swift
@@ -18,37 +18,50 @@ let project = Project(
             )
         ]),
     targets: [
-        .target(name: "Cork", destinations: [.mac], product: .app, bundleId: "com.davidbures.cork", infoPlist: .file(path: "Cork/Info.plist"), sources: [
-            "Cork/**/*.swift"
-        ], resources: [
-            "Cork/**/*.xcassets",
-            "Cork/**/*.xcstrings",
-            "PrivacyInfo.xcprivacy",
-            "Cork/Logic/Helpers/Programs/Sudo Helper"
-        ], dependencies: [
-            // .target(name: "CorkHelp"),
-            .external(name: "LaunchAtLogin"),
-            .external(name: "DavidFoundation")
-        ], settings: .settings(configurations: [
-            .debug(
-                name: "Debug",
-                xcconfig: .relativeToRoot("xcconfigs/Cork.xcconfig")
-            ),
-            .release(
-                name: "Release",
-                xcconfig: .relativeToRoot("xcconfigs/Cork.xcconfig")
-            )
-        ])),
-        .target(name: "CorkHelp", destinations: [.mac], product: .bundle, bundleId: "com.davidbures.corkhelp", settings: .settings(configurations: [
-            .debug(
-                name: "Debug",
-                xcconfig: .relativeToRoot("xcconfigs/CorkHelp.xcconfig")
-            ),
-            .release(
-                name: "Release",
-                xcconfig: .relativeToRoot("xcconfigs/CorkHelp.xcconfig")
-            )
-        ])),
+        .target(
+            name: "Cork",
+            destinations: [.mac],
+            product: .app,
+            bundleId: "com.davidbures.cork",
+            infoPlist: .file(path: "Cork/Info.plist"),
+            sources: [
+                "Cork/**/*.swift"
+            ], resources: [
+                "Cork/**/*.xcassets",
+                "Cork/**/*.xcstrings",
+                "PrivacyInfo.xcprivacy",
+                "Cork/Logic/Helpers/Programs/Sudo Helper"
+            ], dependencies: [
+                // .target(name: "CorkHelp"),
+                .external(name: "LaunchAtLogin"),
+                .external(name: "DavidFoundation")
+            ], settings: .settings(configurations: [
+                .debug(
+                    name: "Debug",
+                    xcconfig: .relativeToRoot("xcconfigs/Cork.xcconfig")
+                ),
+                .release(
+                    name: "Release",
+                    xcconfig: .relativeToRoot("xcconfigs/Cork.xcconfig")
+                )
+            ])
+        ),
+        .target(
+            name: "CorkHelp",
+            destinations: [.mac],
+            product: .bundle,
+            bundleId: "com.davidbures.corkhelp",
+            settings: .settings(configurations: [
+                .debug(
+                    name: "Debug",
+                    xcconfig: .relativeToRoot("xcconfigs/CorkHelp.xcconfig")
+                ),
+                .release(
+                    name: "Release",
+                    xcconfig: .relativeToRoot("xcconfigs/CorkHelp.xcconfig")
+                )
+            ])
+        ),
     ],
     schemes: [
         .scheme(
