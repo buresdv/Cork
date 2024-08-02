fcd99c0ba35e6acdc83acc61e662e8be90d4235e
David Bureš <buresdv@gmail.com>
2024-08-02 18:14:01 +0200

    ^ Outdated package tracker animation fix

--- Cork/CorkApp.swift
+++ Cork/CorkApp.swift
@@ -230,7 +230,10 @@ struct CorkApp: App
                     AppConstants.logger.debug("Number of displayable outdated packages changed (\(outdatedPackageCount))")
                     
                     // TODO: Remove this once I figure out why the updating spinner sometimes doesn't disappear
-                    appDelegate.appState.isCheckingForPackageUpdates = false
+                    withAnimation
+                    {
+                        appDelegate.appState.isCheckingForPackageUpdates = false
+                    }
 
                     if outdatedPackageCount == 0
                     {
