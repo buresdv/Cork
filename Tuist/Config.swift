aa2a11b2bf14b986a7d9725e2c5e1d6d3d7b7c7b
David Bureš <buresdv@gmail.com>
2024-08-04 18:56:22 +0200

    ~ Version Bump

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -2,8 +2,8 @@ import ProjectDescription
 
 let config = Config(
     compatibleXcodeVersions: .upToNextMajor("15"),
-    fullHandle: "buresdv/cork",
-    url: "https://cloud.tuist.io",
+    //fullHandle: "buresdv/cork",
+    //url: "https://cloud.tuist.io",
     swiftVersion: "5.9.0",
     generationOptions: .options()
 )
