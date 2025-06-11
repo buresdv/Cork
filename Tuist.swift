1274c07e2df13a686bc66d20835aded0a87e611c
David Bureš - Virtual <davidbures@David--Virtualni-pocitac.local>
2025-06-11 22:11:36 +0200

    + Settings resizing animation
    # Conflicts:
    #	Tuist.swift

--- Tuist.swift
+++ Tuist.swift
@@ -2,10 +2,7 @@ import ProjectDescription
 
 let config: Config = .init(
     project: .tuist(
-        compatibleXcodeVersions: .list([
-            .upToNextMajor(.init(16, 0, 0)),
-            .upToNextMajor(.init(26, 0, 0))
-        ]),
+        compatibleXcodeVersions: .upToNextMajor(.init(26, 0, 0)),
         swiftVersion: .init(6, 0, 0),
         plugins: .init(),
         generationOptions: .options(),
