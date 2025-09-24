f2718518a1c630ae7cfea47871ec8769c236fe51
Mike Cunneen <mike@appworkshop.net>
2025-09-24 10:52:39 +0800

    ~ support xcode 26.0.1
    Fixes #523 ("The selected Xcode version is 26.0.1, which is not compatible with this project's Xcode version requirement of 16.0.0..<17.0.0.")

--- Tuist/Tuist.swift
+++ Tuist/Tuist.swift
@@ -1,7 +1,7 @@
 import ProjectDescription
 
 let config = Config(
-    compatibleXcodeVersions: .upToNextMajor("16"),
+    compatibleXcodeVersions: .upToNextMajor("26"),
     // fullHandle: "buresdv/cork",
     // url: "https://cloud.tuist.io",
     swiftVersion: .init(6, 0, 0),
