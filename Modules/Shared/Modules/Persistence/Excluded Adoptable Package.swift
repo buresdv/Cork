99bbe6eea7e3a5542223a972ef14e9a084a6efa7
David Bureš <buresdv@gmail.com>
2025-11-14 15:55:17 +0100

    ~ WIP app exclusion (doesn't work)

--- Modules/Shared/Modules/Persistence/Excluded Adoptable Package.swift
+++ Modules/Shared/Modules/Persistence/Excluded Adoptable Package.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+public final class ExcludedAdoptableApp
+{
+    @Attribute(.unique) @Attribute(.spotlight)
+    public var appExecutable: String
+    
+    public init(appExecutable: String)
+    {
+        self.appExecutable = appExecutable
+    }
+    
+    @MainActor
+    public func saveSelfToDatabase()
+    {
+        AppConstants.shared.logger.debug("Will try to insert \(self.appExecutable) to excluded app database")
+        AppConstants.shared.modelContainer.mainContext.insert(self)
+    }
+
+    @MainActor
+    public func deleteSelfFromDatabase()
+    {
+        let modelContext: ModelContext = AppConstants.shared.modelContainer.mainContext
+
+        do
+        {
+            let descriptor = FetchDescriptor<ExcludedAdoptableApp>(
+                predicate: #Predicate { $0.appExecutable == appExecutable }
+            )
+
+            if let existingPackage = try modelContext.fetch(descriptor).first
+            {
+                modelContext.delete(existingPackage)
+            }
+        }
+        catch
+        {
+            AppConstants.shared.logger.error("Failed to fetch excluded adoptable app for deletion: \(error.localizedDescription)")
+        }
+    }
+}
