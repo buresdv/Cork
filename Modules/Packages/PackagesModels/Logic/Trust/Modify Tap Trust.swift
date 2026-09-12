5f8bf2779209c18987f08ec7b6998dc48852313e
David Bureš <buresdv@gmail.com>
2026-09-12 14:15:47 +0200

    + Start of tap trust logic writing

--- Modules/Packages/PackagesModels/Logic/Trust/Modify Tap Trust.swift
+++ Modules/Packages/PackagesModels/Logic/Trust/Modify Tap Trust.swift
@@ -7,6 +7,39 @@
 
 import Foundation
 import CorkShared
-import CorkModels
+import FactoryKit
+import CorkTerminalFunctions
 
-
+public extension BrewTap
+{
+    enum TapTrustModificationError: LocalizedError
+    {
+        case unimplemented(error: [TerminalOutput])
+        case implemented(ImplementedError)
+        
+        public enum ImplementedError: LocalizedError
+        {
+            case coultNotRetrievePointerToThisTapInTrustTracker
+        }
+    }
+    
+    func trustSelf() async throws(TapTrustModificationError)
+    {
+        
+        guard var thisTapInTrustTracker: BrewTapName = Container.shared.trustTracker.resolve().trustedTapNames.first(where: { $0 == self.nameInternal }) else {
+            throw .implemented(.coultNotRetrievePointerToThisTapInTrustTracker)
+        }
+        
+        
+    }
+    
+    enum TapTrustModificationWritingError: LocalizedError
+    {
+        
+    }
+    
+    private func writeTapTrustModificationStatusToFile() async throws
+    {
+        
+    }
+}
