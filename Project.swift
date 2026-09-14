50880eafbe345234f704b635ba4b5f0d03abf17f
David Bureš <buresdv@gmail.com>
2026-09-14 12:56:57 +0200

    ~ Version bump
    fixed the broken double-click

--- Project.swift
+++ Project.swift
@@ -2,8 +2,8 @@ import ProjectDescription
 
 let settings = Environment.selfCompiled.getBoolean(default: false)
 
-let version: String = "2.0.2"
-let build: String = "145"
+let version: String = "2.0.3"
+let build: String = "146"
 
 func corkTarget(configureWithSelfCompiled: Bool) -> ProjectDescription.Target {
     var additionalCompilationConditions = [String]()
